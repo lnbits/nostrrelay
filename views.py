30150c456114d267e2674baf02f6acec76abab03
Pavol Rusnak <pavol@rusnak.io>
2023-01-30 18:22:06 +0000

    remove unused imports, don't use format string when not needed

--- views.py
+++ views.py
@@ -1,8 +1,5 @@
-from http import HTTPStatus
-
 from fastapi import Depends, Request
 from fastapi.templating import Jinja2Templates
-from starlette.exceptions import HTTPException
 from starlette.responses import HTMLResponse
 
 from lnbits.core.models import User
